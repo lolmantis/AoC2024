11045f158383a79581f3e3c84bc8c7cddcb95987
lolmantis <86570299+lolmantis@users.noreply.github.com>
2024-12-14 02:42:59 +0000

    Revert "Day 13"
    This reverts commit 5f269ecaf02ef4f8abb39df6bcb560e0d4fc8063.

--- src/runner.java
+++ src/runner.java
@@ -1,12 +1,12 @@
 public class runner {
   public static void main(String[] args) {
 
-    Day13 current = new Day13();
+    Day12 current = new Day12();
 
-    current.Solve(true, false, false);
+//    current.Solve(true, false, false);
     current.Solve(true,true, true);
-    current.Solve(false,false, false);
-    current.Solve(false,true);
+//    current.Solve(false,false, false);
+//    current.Solve(false,true);
 
   }
 }
